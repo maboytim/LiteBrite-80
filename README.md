7ae9489b4e5b175cd5cd977592232144aadaff9d
Matt Boytim <maboytim@yahoo.com>
2025-07-20 19:35:41 -0400

    Added kicad design.  Added demo programs.  Updated readme.

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # LiteBrite-80
-Memory visualization gadget for Z80 systems.
+
+The LiteBrite-80 (LB-80) is a memory visualization gadget for Z80 systems.  It displays the 1k (8x8 display) or 256 (16x16 display) byte block being accessed by the Z80 (code and data read/write).  It is primarily intended for Z80 based TRS-80 systems (Models 1, 2 (12, etc), 3, and 4) but is applicable to any Z80 based system.
+
+The LB-80 consists of two parts - a Z80 adapter and a display.  The Z80 adapter is a small PCB in which the Z80 is installed and replaces the Z80.  Ideally the Z80 is socketed in the host machine and the LB-80 is just installed in the socket - otherwise the Z80 must be removed and the LB-80 installed in its place or replaced with a socket.  The adapter has a connector to attach a display.  The display is a small PCB that contains an 8x8 or 16x16 LED array that can either plug directly into the adapter or more likely connected to the adapter via a cable so that the display can be located for convenient viewing.
+
+## Adapter PCB
+
+The adapter PCB design is available [here](kicad/Visual80-3).  The design is intended to be as compact as possible so to fit into about the space of the Z80 itself.  To achieve this the design is essentially a 40-pin socket - but with a small protrusion for the display connector.  The adapter could be used in place of a Z80 socket, or if installed in a socket is equivalent to stacking a second socket on top of the socket.
+
+The design uses individual machine socket pins which can be removed from a machine socket or bare pins can be purchased.  The pins should be inserted in the PCB until the collar touches the top of the PCB - they might have a slight interference fit and need to be pressed into the PCB.
+
+The connector pinout is compatible with the PMOD connector common to FPGA development boards.  The PMOD connector is simply a 2x6 connector with two GND pins, two Vcc pins, and eight signal pins.  For an 8x8 display only six signals are needed so a pinout that allows for a 10-pin connector for 8x8 display is used - this allows for use of a 10-pin cable when connecting the display via a cable.
+
+The use of the PMOD connector allows the display to be used with many FPGA boards for example if emulating a TRS-80 with an FPGA.
+
+## Display PCB
+
+An 8x8 display PCB design is available in the [TRS-IO project](https://github.com/apuder/TRS-IO/tree/master/kicad/pmod/lb-80).
+
+## Software
+
+No special software is needed because the LB-80 just displays the natural behavior of the Z80 activity.  The resultant patterns are interesting and fun to watch but also often lend insight into what the Z80 is doing.
+
+However, it's possible to contrive a program that generates certain patterns on the LB-80 display.  To light an individual dot on an 8x8 display requires that the Z80 be limited to the corresponding 1k block of memory (program, data, and stack).  To light a different individual LED would require the Z80 be limited to a different corresponding 1k block of memory.  To not light an LED would require the Z80 not touch the corresponding 1k block of memory.  So contriving such a program would be tricky.
+
+### Marquee Display Program
+
+A TRS-80 program [lb80text](source/lb80text) that displays scrolling marquee style text on the LB-80 is provided as one such example.  See the source code for detailed explanation of how the program works.  Several versions are provided.
+
+The command syntax is `lb80text "string to display"`.  The quotes are optional and only required if the string has leading spaces.
+
+It happens this demo program is a good stress case for memory.  Although using additional banked memory in the M4 and M2 really has no effect on the operation of the LB-80 itself some banked versions are included.
+
+#### [TRS-80 Model 1 and Model 3 Version](source/lb80text/lb80txm1.asm)
+
+For TRS-80 M1 with lowercase mod and 48k RAM, and M3 with 48k.  See source code for M1 without lowercase mod.
+
+#### [TRS-80 Model 4 Version](source/lb80text/lb80txm4.asm)
+
+For TRS-80 M4 with 64k.
+
+#### [TRS-80 Model 2 Version](source/lb80text/lb80txm2.asm)
+
+For TRS-80 M2 with 64k.  Very similar to M4 version but doesn't use display memory since the M2 can't run from display memory.
+
+#### TRS-80 Model 4 128k Version
+
+Coming...
+
+#### [TRS-80 Model 2 512k Version](source/lb80text/lb80512k.asm)
+
+For TRS-80 M2 with 512k.  A M2 (M12, etc) with 512k is a rare beast but if you are one of the lucky few with a [512k card by Ruud](https://www.youtube.com/watch?v=_I01NHzUzNk) this will give you something to test it with.  (Note: I also have a 512k card of my own design that's not yet ready for primetime).
+
+## Alternative Adapter Implementations
+
+In systems that expose the Z80 bus an LB-80 adapter that connects to the bus rather than directly to the Z80 is possible.  Such systems include:
+* TRS-80 Model 1
+* TRS-80 Model 2 (12, etc)
+* S100
---
+++ source/lb80text/lb80txm4.asm
@@ -0,0 +1,347 @@
+; Assemble with Matthew Reed's z80 assembler
+; http://www.trs-80emulators.com/z80asm/
+;
+; z80asm.exe lb80txm4.asm
+
+
+; Program to display text string in scrolling marquee style on LB-80 8x8 display.
+; The code moves continuously through memory and runs in the region corresponding
+; to the LED (dot) to be lit.  It copies itself to the region corresponding to
+; then the next dot to be lit and then jumps there.  Then repeats for the next
+; dot to be lit, and so on.  At the end of the string it loops back to the
+; beginning and repeats indefinitely until reset.
+;
+; TRS-80 Model 4 version
+;
+; The TRS-80 M4 has display and keyboard optionally mapped at F800h and F400h
+; respectively.  However they are enabled/disabled together so it's not possible
+; to enable only the display but not the keyboard.  The Z80 can run from the
+; display memory but not the keyboard.  The display and keyboard could just be
+; disabled but including the display memory is more interesting - this requires a 
+; 'gratuitous move' for the keyboard block (see the M1/M3 version for explanation
+; of gratuitous move).
+;
+; The program, data, and stack need to be less than 1k to fit completely within
+; a region that corresponds to a single dot.  The copy is performed usin a LDIR
+; block move instruction.  The rate at which the program steps through the dots
+; will depend on the size of the block move.  The size could be as small as the
+; program and data but the rate of stepping through the dots would depend on
+; the size.  Instead a fixed LDIR size is used but of course need to be at least
+; as big as the progam and data.
+;
+; The program first converts the display string to dot data and then iterates
+; over the dot data.  The part of the program that does this isn't needed after
+; conversion is done so isn't included in the portion that moves throughout
+; memory.  The portion that moves throught memory starts at lb80txt.
+;
+; This LB-80 demo program turns out to be a pretty good memory stess test.  Any
+; memory errors are likely to crash the program or at least have some visual
+; effect on what is displayed.  To exercise memory even further the program moves
+; itself within the 1k blocks.  It advances one byte with each copy until it
+; would no longer fit and then jumps back.  If the display memory is incuded then
+; this movement can be see on the display.
+
+; iterations of the ldir
+; This needs to be at least enough to contain the lb80txt code and data.
+; The time this takes will affect the scroll rate - increase to slow down the
+; scrolling, decrease to increase the scrolling.  The value here was determined
+; empirically.
+;
+itern   equ     0180h
+
+
+        org     4000h
+
+entry   ld      a,(hl)          ;skip an opening " - this is so string can start with space
+        cp      '"'
+        jr      nz,noquot
+        inc     hl
+        ld      a,(hl)
+noquot  cp      ' '             ;loop until ctrl char
+        jr      nc,gotarg
+        ld      hl,deftxt       ;if end of line then use default string
+gotarg  ld      de,coldta
+arglp   ld      a,(hl)
+        sub     ' '             ;expand characte to 6 columns of dots
+        jr      c,argend
+        push    hl
+        ld      l,a
+        ld      h,0
+        add     hl,hl   ;*2
+        add     hl,hl   ;*4
+        ld      c,a
+        ld      b,0
+        add     hl,bc   ;*5
+        ld      bc,chrdta
+        add     hl,bc
+        ld      bc,5
+        ldir
+        xor     a
+        ld      (de),a
+        inc     de
+        pop     hl
+        inc     hl
+        jr      arglp
+argend
+        ld      hl,-coldta      ;compute length
+        add     hl,de
+; the display routine will go 7 col's past its len so sub 7
+; so the len must be at least 8 but if the string is only
+; a single character the len would be only 6 so pad up for
+; that case
+        ld      bc,-8
+        add     hl,bc
+        jr      c,nopad
+padlen  xor     a
+        ld      (de),a
+        inc     de
+        inc     hl
+        ld      a,h
+        or      l
+        jr      nz,padlen
+nopad   inc     hl
+
+        ld      (len+1),hl      ;save len
+
+; fill remainder of data area with *'s
+filldta ld      a,'*'
+        ld      (de),a
+        inc     de
+        inc     hl
+        ld      a,h
+        cp      itern .shr. 8
+        jr      c,filldta
+        ld      a,l
+        cp      itern .and. 0FFh
+        jr      c,filldta
+
+        di
+        ld      a,86h           ;enable display and keyboard
+        out     (84h),a   
+        ld      sp,lb80txt+itern;put stack at end of block
+        ld      ix,lb80txt      ;jump to actual code
+        jp      (ix)
+
+; default string to display
+deftxt  defb    " Say hi to LB-80-M4! ",0
+
+; 5x8 character data as columns of dots
+chrdta
+        defb    000h,000h,000h,000h,000h ;  
+        defb    000h,000h,0FAh,000h,000h ; !
+        defb    000h,0E0h,000h,0E0h,000h ; "
+        defb    028h,0FEh,028h,0FEh,028h ; #
+        defb    024h,054h,0FEh,054h,048h ; $
+        defb    0C4h,0C8h,010h,026h,046h ; %
+        defb    06Ch,092h,06Ah,004h,00Ah ; &
+        defb    010h,0E0h,0C0h,000h,000h ; '
+        defb    000h,038h,044h,082h,000h ; (
+        defb    000h,082h,044h,038h,000h ; )
+        defb    054h,038h,0FEh,038h,054h ; *
+        defb    010h,010h,07Ch,010h,010h ; +
+        defb    001h,00Eh,00Ch,000h,000h ; ,
+        defb    010h,010h,010h,010h,010h ; -
+        defb    000h,006h,006h,000h,000h ; .
+        defb    004h,008h,010h,020h,040h ; /
+        defb    07Ch,08Ah,092h,0A2h,07Ch ; 0
+        defb    000h,042h,0FEh,002h,000h ; 1
+        defb    04Eh,092h,092h,092h,062h ; 2
+        defb    044h,082h,092h,092h,06Ch ; 3
+        defb    018h,028h,048h,0FEh,008h ; 4
+        defb    0E4h,0A2h,0A2h,0A2h,09Ch ; 5
+        defb    03Ch,052h,092h,092h,00Ch ; 6
+        defb    086h,088h,090h,0A0h,0C0h ; 7
+        defb    06Ch,092h,092h,092h,06Ch ; 8
+        defb    060h,092h,092h,094h,078h ; 9
+        defb    000h,06Ch,06Ch,000h,000h ; :
+        defb    001h,06Eh,06Ch,000h,000h ; ;
+        defb    010h,028h,044h,082h,000h ; <
+        defb    028h,028h,028h,028h,028h ; =
+        defb    000h,082h,044h,028h,010h ; >
+        defb    040h,080h,08Ah,090h,060h ; ?
+        defb    04Ch,092h,09Eh,082h,07Ch ; @
+        defb    03Eh,048h,088h,048h,03Eh ; A
+        defb    082h,0FEh,092h,092h,06Ch ; B
+        defb    07Ch,082h,082h,082h,044h ; C
+        defb    082h,0FEh,082h,082h,07Ch ; D
+        defb    0FEh,092h,092h,082h,082h ; E
+        defb    0FEh,090h,090h,080h,080h ; F
+        defb    07Ch,082h,082h,092h,09Eh ; G
+        defb    0FEh,010h,010h,010h,0FEh ; H
+        defb    000h,082h,0FEh,082h,000h ; I
+        defb    004h,002h,002h,002h,0FCh ; J
+        defb    0FEh,010h,028h,044h,082h ; K
+        defb    0FEh,002h,002h,002h,002h ; L
+        defb    0FEh,040h,030h,040h,0FEh ; M
+        defb    0FEh,040h,020h,010h,0FEh ; N
+        defb    07Ch,082h,082h,082h,07Ch ; O
+        defb    0FEh,090h,090h,090h,060h ; P
+        defb    07Ch,082h,08Ah,084h,07Ah ; Q
+        defb    0FEh,090h,098h,094h,062h ; R
+        defb    064h,092h,092h,092h,04Ch ; S
+        defb    080h,080h,0FEh,080h,080h ; T
+        defb    0FCh,002h,002h,002h,0FCh ; U
+        defb    0E0h,018h,006h,018h,0E0h ; V
+        defb    0FEh,004h,008h,004h,0FEh ; W
+        defb    0C6h,028h,010h,028h,0C6h ; X
+        defb    0C0h,020h,01Eh,020h,0C0h ; Y
+        defb    086h,08Ah,092h,0A2h,0C2h ; Z
+        defb    020h,040h,0FEh,040h,020h ; [
+        defb    008h,004h,0FEh,004h,008h ; \
+        defb    010h,038h,054h,010h,010h ; ]
+        defb    010h,010h,054h,038h,010h ; ^
+        defb    002h,002h,002h,002h,002h ; _
+        defb    000h,000h,0E0h,0D0h,000h ; `
+        defb    004h,02Ah,02Ah,02Ah,01Eh ; a
+        defb    0FEh,014h,022h,022h,01Ch ; b
+        defb    01Ch,022h,022h,022h,014h ; c
+        defb    01Ch,022h,022h,014h,0FEh ; d
+        defb    01Ch,02Ah,02Ah,02Ah,018h ; e
+        defb    000h,010h,07Eh,090h,040h ; f
+        defb    018h,025h,025h,019h,03Eh ; g
+        defb    0FEh,010h,020h,020h,01Eh ; h
+        defb    000h,022h,0BEh,002h,000h ; i
+        defb    002h,001h,001h,001h,0BEh ; j
+        defb    0FEh,008h,014h,022h,000h ; k
+        defb    000h,082h,0FEh,002h,000h ; l
+        defb    03Eh,020h,01Eh,020h,01Eh ; m
+        defb    03Eh,010h,020h,020h,01Eh ; n
+        defb    01Ch,022h,022h,022h,01Ch ; o
+        defb    03Fh,018h,024h,024h,018h ; p
+        defb    018h,024h,024h,018h,03Fh ; q
+        defb    03Eh,010h,020h,020h,010h ; r
+        defb    012h,02Ah,02Ah,02Ah,024h ; s
+        defb    020h,020h,0FCh,022h,024h ; t
+        defb    03Ch,002h,002h,004h,03Eh ; u
+        defb    038h,004h,002h,004h,038h ; v
+        defb    03Ch,002h,00Ch,002h,03Ch ; w
+        defb    022h,014h,008h,014h,022h ; x
+        defb    038h,005h,005h,005h,03Eh ; y
+        defb    022h,026h,02Ah,032h,022h ; z
+        defb    000h,010h,06Ch,082h,000h ; {
+        defb    000h,000h,0EEh,000h,000h ; |
+        defb    000h,082h,06Ch,010h,000h ; }
+        defb    040h,080h,040h,020h,040h ; ~
+        defb    054h,0AAh,054h,0AAh,054h ; 
+
+
+; actual display code
+; this needs to be 1k aligned
+; it wouldn't need to be but if it's in the destination region
+; of a copy it will get clobbered
+
+        org     4800h
+
+lb80txt ld      hl,coldta-lb80txt ;get start of column data
+len     ld      bc,$-$          ;get length
+
+lp0     push    bc              ;save length
+        defb    0DDh            ;ld e,ixl
+        ld      e,l
+        defb    0DDh            ;ld a,ixh
+        ld      a,h
+        and     3               ;get start address of first column
+        ld      d,a
+        ex      de,hl           ;de=column data index, hl=column address
+        ld      c,8             ;get column count
+
+lp1     push    ix              ;compute pointer to column data
+        ex      (sp),hl
+        add     hl,de
+        ld      a,(hl)          ;get next column data
+        pop     hl
+        push    de              ;save column data index
+
+        ld      b,8             ;get row count
+lp2     push    bc              ;save row and column count
+        ld      bc,itern
+        rra
+        jr      nc,pxoff        ;go if pixel off
+
+; hl contains the computed base destination
+; ix contains the current base
+pxon    ld      e,a
+        ld      a,h
+        cp      0F8h            ;compare dest with display ram start
+        ld      a,e
+        jr      nc,pxondsp
+        ld      a,h
+        cp      0F4h            ;compare dest with keyboard start
+        ld      a,e
+        jr      nc,gratu        ;do gratuitous move if not ram
+pxondsp
+        ld      a,h             ;check for copy to self
+        and     0FCh
+        ld      d,a
+        defb    0DDh            ;ld a,ixh - get current base hi in a
+        ld      a,h
+        and     0FCh
+        cp      d
+        jr      z,nowrap
+
+        inc     hl              ;bump dest and wrap back to 0
+        ld      a,h
+        and     400h-1 .shr. 8
+        cp      400h-itern .shr. 8
+        jr      c,nowrap
+        ld      a,l
+        cp      400h-itern .and. 0FFh
+        jr      c,nowrap
+        ld      a,h
+        and     -400h .shr. 8
+        ld      h,a
+        ld      l,0
+nowrap  ld      a,e
+
+        defb    0DDh            ;ld e,ixl - get current base in de
+        ld      e,l
+        defb    0DDh            ;ld d,ixh
+        ld      d,h
+        ex      de,hl           ;swap so old base hl, new base in de
+        defb    0DDh            ;ld ixl,e - copy new base to ix
+        ld      l,e
+        defb    0DDh            ;ld ixh,d
+        ld      h,d
+        ldir
+
+        ld      hl,-6           ;compute new stack location
+        add     hl,de
+        ld      sp,hl           ;set new stack location
+                                ;jump to the program at the new location
+        ld      hl,pxmrg-lb80txt-itern ;jump to pxmrg at new location
+        add     hl,de
+        jp      (hl)
+
+gratu   ld      e,l
+        ld      d,h
+        ldir
+
+pxmrg   ld      l,e
+        ld      h,d
+
+pxoff   add     hl,bc           ;skip row if jr'ed to here
+        ld      bc,2000h-itern  ;jump to next row
+        add     hl,bc
+        pop     bc              ;restore row and column count
+        djnz    lp2             ;do all rows
+        
+        ld      de,0400h        ;jump to next column
+        add     hl,de
+        pop     de              ;restore column data pointer
+        inc     de              ;bump
+        dec     c               ;do all columns
+        jr      nz,lp1 
+
+        pop     bc              ;restore len
+        ld      hl,-7           ;reset column data pointer to previous start plus 1
+        add     hl,de
+        dec     bc              ;do all data bytes
+        ld      a,b
+        or      c
+        jr      nz,lp0
+        jp      (ix)
+
+coldta
+        defb    0
+
+        end     entry
